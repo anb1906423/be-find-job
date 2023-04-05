4121d268a0bd142063b7baca869b594502cf64fc
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-04-05 21:55:12 +0700

    update api cong viec

--- app/routes/CongViec.js
+++ app/routes/CongViec.js
@@ -5,5 +5,11 @@ const CongViecController = require('../controllers/CongViec.controller')
 
 router.get('/:id', CongViecController.traVeCongViec)
 router.get('/', CongViecController.traVeTatCaCongViec)
+router.post('/create', CongViecController.DangBaiTuyenDung)
+router.put('/update/:id', CongViecController.capNhatCongViec)
+
+router.delete('/delete', CongViecController.xoaCongViec)
+router.put('/on', CongViecController.onState)
+router.put('/off', CongViecController.offState)
 
 module.exports = router
