9deb8cb8af18e17dbe372dc6c58948d58ee2d213
Truongsondev <trongsonpt.80@gmail.com>
2023-04-07 21:59:29 +0700

    fix

--- app/routes/CongViec.js
+++ app/routes/CongViec.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const CongViecController = require('../controllers/CongViec.controller')
+const CongViecController = require("../controllers/CongViec.controller");
 
-router.get('/bai-dang-cong-ty', CongViecController.traVeTatCaCongViecCuaNhaTuyenDung)
-router.get('/', CongViecController.traVeTatCaCongViec)
-router.get('/:id', CongViecController.traVeCongViec)
-router.post('/create', CongViecController.DangBaiTuyenDung)
-router.put('/update/:id', CongViecController.capNhatCongViec)
+router.get(
+    "/bai-dang-cong-ty",
+    CongViecController.traVeTatCaCongViecCuaNhaTuyenDung
+);
+router.get("/", CongViecController.traVeTatCaCongViec);
+router.get("/:id", CongViecController.traVeCongViec);
+router.post("/create", CongViecController.DangBaiTuyenDung);
+router.put("/update/:id", CongViecController.capNhatCongViec);
 
-router.delete('/delete', CongViecController.xoaCongViec)
-router.put('/on', CongViecController.onState)
-router.put('/off', CongViecController.offState)
+router.delete("/delete/:id", CongViecController.xoaCongViec);
+router.put("/on", CongViecController.onState);
+router.put("/off", CongViecController.offState);
 
-module.exports = router
+module.exports = router;
